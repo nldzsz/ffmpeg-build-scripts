b26e329481c665efd0180c5d5d69ac818ed215c1
nldzsz <394360192@qq.com>
2020-08-02 14:53:12 +0800

    重新优化android脚本

--- compile-android.sh
+++ compile-android.sh
@@ -21,10 +21,23 @@ set -e
 # 通过. xx.sh的方式执行shell脚本，变量会被覆盖
 . ./common.sh
 
-# 由于目前设备基本都是电脑64位 手机64位 所以这里脚本默认只支持 arm64 x86_64两个平台
-# FF_ALL_ARCHS_IOS="armv7 armv7s arm64 i386 x86_64"
-export FF_ALL_ARCHS_IOS="arm64e arm64 x86_64"
-target_ios=10.0
+export FF_ALL_ARCHS_ANDROID="armv7a arm64"
+# 编译的API级别 (最小5.0以上系统)
+export FF_ANDROID_API=21
+# 根据实际情况填写ndk路径;(备注:mac和linux平台下，如果从小于19和19以上版本之间切换过ndk版本，那么最好先删掉android/forksource目录重新编译拉取代码，
+# 否则编译fdk-aac时会出现libtool执行错误,导致编译结束)
+# windows，linux，mac平台有各自对应的ndk版本下载地址 https://developer.android.google.cn/ndk/downloads
+#export NDK_PATH=C:/cygwin64/home/Administrator/android-ndk-r21b
+export NDK_PATH=/Users/apple/devoloper/mine/android/android-ndk-r17c
+#export NDK_PATH=/Users/apple/devoloper/mine/android/android-ndk-r20b
+#export NDK_PATH=/Users/apple/devoloper/mine/android/android-ndk-r21b
+#export NDK_PATH=/home/zsz/android-ndk-r20b
+# 开启编译动态库，默认开启
+export FF_COMPILE_SHARED=TRUE
+# 开启编译静态库,默认关闭,动态库和静态库同时只能开启一个，不然导入android使用时会出错
+export FF_COMPILE_STATIC=FALSE
+# windows下统一用bat脚本来生成独立工具编译目录(因为低于18的ndk库中的make_standalone_toolchain.py脚本在cygwin中执行会出错)
+export WIN_PYTHON_PATH=C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/python.exe
 
 # 是否编译这些库;如果不编译将对应的值改为FALSE即可；如果ffmpeg对应的值为TRUE时，还会将其它库引入ffmpeg中，否则单独编译其它库
 export LIBFLAGS=(
@@ -42,53 +55,167 @@ FF_TARGET=$1
 # 配置交叉编译环境
 set_toolchain_path()
 {
-    local ARCH=$1
-    local PLATFORM=
-    case $ARCH in
-    arm64|arm64e)
-        local PLATFORM=iphoneos
-    ;;
-    x86_64)
-        local PLATFORM=iphonesimulator
-    ;;
+    local FF_ARCH=$1
+    local IJK_NDK_REL=$(grep -o '^Pkg\.Revision.*=[0-9]*.*' $NDK_PATH/source.properties 2>/dev/null | sed 's/[[:space:]]*//g' | cut -d "=" -f 2)
+    # 开始编译 pwd代表的执行该脚本脚本的所在目录(不一定是该脚本所在目录)
+    export WORK_PATH=`pwd`
+    case "$uname" in
+        Darwin)
+            export FF_MAKE_FLAGS=-j`sysctl -n machdep.cpu.thread_count`
+        ;;
+        CYGWIN_NT-*)
+            IJK_WIN_TEMP="$(cygpath -am /tmp)"
+            export TEMPDIR=$IJK_WIN_TEMP/
+            echo "Cygwin temp prefix=$IJK_WIN_TEMP/"
+            export WORK_PATH="$(cygpath -am `pwd`)"
+        ;;
     esac
-    mkdir -p ${UNI_BUILD_ROOT}/build/ios-$ARCH/pkgconfig
     
-    # xcrun 是调用iOS交叉编译工具的命令 通过xcrun --help 可以查看具体使用，后面跟具体的编译工具 如ar cc等等
-    # 定义编译工具CC CXX AR等必须要用export进行声明，否则没有效果;-f代表输出对应工具的绝对路径;如下CC和CXX只要直接使用clang和clang++(否则编译mp3lame库会出错，原因未知:fixbug)
-    export CC=clang
-    export CXX=clang++
-    export AR=$(xcrun --sdk $PLATFORM -f ar)
-    export OBJC=$(xcrun --sdk $PLATFORM -f clang)
-    export LD=$(xcrun --sdk $PLATFORM -f ld)
-    export RANLIB=$(xcrun --sdk $PLATFORM -f ranlib)
-    export STRIP=$(xcrun --sdk $PLATFORM -f strip)
-    export SDKPATH=$(xcrun --sdk $PLATFORM --show-sdk-path)
-    export PKG_CONFIG_LIBDIR="${UNI_BUILD_ROOT}/build/ios-$ARCH/pkgconfig"
-    export ZLIB_PACKAGE_CONFIG_PATH="${PKG_CONFIG_LIBDIR}/zlib.pc"
-    export BZIP2_PACKAGE_CONFIG_PATH="${PKG_CONFIG_LIBDIR}/bzip2.pc"
-    export LIB_ICONV_PACKAGE_CONFIG_PATH="${PKG_CONFIG_LIBDIR}/libiconv.pc"
-    export LIB_UUID_PACKAGE_CONFIG_PATH="${PKG_CONFIG_LIBDIR}/uuid.pc"
+    FF_SYSROOT=""
+    FF_CROSS_PREFIX=
+    local FF_CC_CPP_PREFIX=
+    local FF_ARCH_1=arm
+    local FF_CC=gcc
+    local FF_CPP=g++
+    local FF_HOST_OS=
+    if [ "$FF_ARCH" = "armv7a" ]; then
+        FF_ARCH_1=arm
+        FF_CROSS_PREFIX=arm-linux-androideabi
+        FF_CC_CPP_PREFIX=armv7a-linux-androideabi$FF_ANDROID_API
+    elif [ "$FF_ARCH" = "arm64" ]; then
+        FF_ARCH_1=arm64
+        FF_CROSS_PREFIX=aarch64-linux-android
+        FF_CC_CPP_PREFIX=aarch64-linux-android$FF_ANDROID_API
+    else
+        echo "unsurport platform !"
+        exit 1
+    fi
+
+    local FF_TOOLCHAIN_PATH=$WORK_PATH/build/forksource/android-toolchain-$FF_ARCH
+    local FF_TOOLCHAIN_TOUCH="$FF_TOOLCHAIN_PATH/touch"
+    local FF_SAVE_NDK_VERSION="1.0"
+    if [ -f "$FF_TOOLCHAIN_TOUCH" ]; then
+        FF_SAVE_NDK_VERSION=`cat "$FF_TOOLCHAIN_TOUCH"`
+    fi
     
-    if [ ! -f ${ZLIB_PACKAGE_CONFIG_PATH} ]; then
-        create_zlib_system_package_config $SDKPATH $PKG_CONFIG_LIBDIR
+    if [ "$FF_SAVE_NDK_VERSION" != "$IJK_NDK_REL" ];then
+        echo "make NDK standalone toolchain...."
+        echo ""
     fi
 
-    if [ ! -f ${LIB_ICONV_PACKAGE_CONFIG_PATH} ]; then
-        create_libiconv_system_package_config $SDKPATH $PKG_CONFIG_LIBDIR
+    # 遇到问题：cygwin编译x264时环境变量不起作用。
+    # 分析原因：对于cyg编译工具的PATH环境变量，x264的编译脚本无法识别C:这样的盘符(它用:/cygdrive/c来表示C盘)
+    # 解决方案：直接指定AR，CC，CPP的绝对路径
+    # 创建独立工具链 参考https://developer.android.com/ndk/guides/standalone_toolchain
+    #export PATH=$FF_TOOLCHAIN_PATH/bin/:$PATH
+    if [[ "$uname" == CYGWIN_NT-* ]]; then
+        
+        if [ "$FF_SAVE_NDK_VERSION" != "$IJK_NDK_REL" ]; then
+            
+            # NDK版本不一样了，则先删除以前的
+            if [ -f "$FF_TOOLCHAIN_TOUCH" ]; then
+                rm -rf $FF_TOOLCHAIN_PATH
+            fi
+            
+            
+            #遇到问题：cyg调用dnk17以下版本的make-standalone-toolchain.sh脚本会出错，而且ndk21的此脚本也是各种问题
+            #分析原因：可能此脚本不同版本的兼容性未做好
+            #解决方案：cyg调用bat脚本来安装独立工具链则很好的解决了兼容性问题
+            #windows的cyg编译则调用bat脚本来安装独立工具链
+            echo "cwgwin windows bat install maketool..."
+            $WORK_PATH/maketool_install.bat "$WIN_PYTHON_PATH" "$NDK_PATH" $FF_ARCH_1 $FF_ANDROID_API "$FF_TOOLCHAIN_PATH"
+            
+            # 避免重复执行make-standalone-toolchain.sh指令
+            touch $FF_TOOLCHAIN_TOUCH;
+            echo "$IJK_NDK_REL" >$FF_TOOLCHAIN_TOUCH
+        fi
+        
+        # 定义cyg的C编译器和CPP编译器
+        FF_CC=clang.cmd
+        FF_CPP=clang++.cmd
+
+        FF_SYSROOT=$FF_TOOLCHAIN_PATH/sysroot
+        FF_CROSS_PREFIX=$FF_TOOLCHAIN_PATH/bin/${FF_CROSS_PREFIX}
+        FF_CC_CPP_PREFIX=$FF_CROSS_PREFIX
+        FF_HOST_OS=windows-x86_64
+    else
+        # 其他系统 mac和linux
+        if [ "$FF_SAVE_NDK_VERSION" != "$IJK_NDK_REL" ]; then
+            
+            
+            # NDK版本不一样了，则先删除以前的
+            if [ -f "$FF_TOOLCHAIN_TOUCH" ]; then
+                rm -rf $FF_TOOLCHAIN_PATH
+            fi
+            
+            # ndk19以前才需要，但是ndk20在ubunto上编译mp3lame时报错，21没问题，所以21以下统一用此方法，21及以上才不用安装独立工具链
+            if [[ "$IJK_NDK_REL" < "21" ]]; then
+                # 该脚本将ndk目录下的编译工具复制到指定的位置，后面./configure配置的时候指定的路径就可以写这里指定的位置了
+                $NDK_PATH/build/tools/make-standalone-toolchain.sh \
+                    --install-dir=$FF_TOOLCHAIN_PATH \
+                    --platform="android-$FF_ANDROID_API" \
+                    --arch=$FF_ARCH_1   \
+                    --toolchain=${FF_CROSS_PREFIX}-4.9
+                    
+                # 避免重复执行make-standalone-toolchain.sh指令
+                touch $FF_TOOLCHAIN_TOUCH;
+                echo "$IJK_NDK_REL" >$FF_TOOLCHAIN_TOUCH
+            fi
+            
+        fi
+        
+        if [ "$uname" == "Linux" ];then
+            FF_HOST_OS=linux-x86_64
+        else
+            FF_HOST_OS=darwin-x86_64
+        fi
+        
+        FF_CC=clang
+        FF_CPP=clang++
+        if [[ "$IJK_NDK_REL" < "21" ]]; then
+            FF_SYSROOT=$FF_TOOLCHAIN_PATH/sysroot
+            FF_CROSS_PREFIX=$FF_TOOLCHAIN_PATH/bin/${FF_CROSS_PREFIX}
+            FF_CC_CPP_PREFIX=$FF_CROSS_PREFIX
+        else
+            # ndk 19以后则直接使用ndk原来的目录即可;而且FF_SYSROOT不需要用--sysroot来指定了，否则编译会出错
+            FF_SYSROOT=""
+            FF_CROSS_PREFIX=$NDK_PATH/toolchains/llvm/prebuilt/$FF_HOST_OS/bin/${FF_CROSS_PREFIX}
+            FF_CC_CPP_PREFIX=$NDK_PATH/toolchains/llvm/prebuilt/$FF_HOST_OS/bin/${FF_CC_CPP_PREFIX}
+        fi
+
     fi
 
-    if [ ! -f ${BZIP2_PACKAGE_CONFIG_PATH} ]; then
-        create_bzip2_system_package_config $SDKPATH $PKG_CONFIG_LIBDIR
-    fi
-
-    if [ ! -f ${LIB_UUID_PACKAGE_CONFIG_PATH} ]; then
-        create_libuuid_system_package_config $SDKPATH $PKG_CONFIG_LIBDIR
-    fi
+    export FF_SYSROOT
+    export FF_CROSS_PREFIX
+    # 编译缓存，可以加快编译
+    #export CC="ccache ${FF_CROSS_PREFIX}-gcc"
+    # fixbug:ndk20版本之后，预编译器cpp已经内置到CC中了，所以如果这里再指定会出现找不到cpp的错误
+    #export CPP=${FF_CROSS_PREFIX}-cpp
+    export AR=${FF_CROSS_PREFIX}-ar
+    # 开启该选项后x264的编译选项 -DSTAK_ALIGNMENT=会加入到AS中，导致编译失败。如果没有定义这个，
+    # -DSTAK_ALIGNMENT=会作为gccmingl的参数，则编译通过
+    #export AS=${FF_CROSS_PREFIX}-as
+    export NM=${FF_CROSS_PREFIX}-nm
+    export CC=${FF_CC_CPP_PREFIX}-$FF_CC
+    export CXX=${FF_CC_CPP_PREFIX}-$FF_CPP
+    export LD=${FF_CROSS_PREFIX}-ld
+    export RANLIB=${FF_CROSS_PREFIX}-ranlib
+    export STRIP=${FF_CROSS_PREFIX}-strip
+    export OBJDUMP=${FF_CROSS_PREFIX}-objdump
+    export OBJCOPY=${FF_CROSS_PREFIX}-objcopy
+    export ADDR2LINE=${FF_CROSS_PREFIX}-addr2line
+    export READELF=${FF_CROSS_PREFIX}-readelf
+    export SIZE=${FF_CROSS_PREFIX}-size
+    export STRINGS=${FF_CROSS_PREFIX}-strings
+    export ELFEDIT=${FF_CROSS_PREFIX}-elfedit
+    export GCOV=${FF_CROSS_PREFIX}-gcov
+    export GDB=${FF_CROSS_PREFIX}-gdb
+    export GPROF=${FF_CROSS_PREFIX}-gprof
+    export PKG_CONFIG_LIBDIR="${UNI_BUILD_ROOT}/build/android-$ARCH/pkgconfig"
 }
 set_flags()
 {
-    ARCH=$1
+    local ARCH=$1
     # 用来配置编译器参数，一般包括如下几个部分：
     # 1、平台cpu架构相关的参数，比如arm64、x86_64不同cpu架构相关的参数也不一样，一般是固定的
     # 2、编译器相关参数，比如std=c99，不同的库所使用的语言以及语言的版本等等
@@ -96,34 +223,33 @@ set_flags()
     # 4、系统路径以及系统版本等相关参数 -isysroot=<SDK_PATH> -I<SDK_PATH>/usr/include
     CFLAGS=
     ASM_FLAGS=
+    HOST=
     if [ $ARCH = "x86_64" ];then
-        PLATFORM="iphonesimulator"
-        CFLAGS="-arch x86_64 -march=x86-64 -msse4.2 -mpopcnt -m64 -mtune=intel"
-        CFLAGS="$CFLAGS -mios-simulator-version-min=$target_ios"
-        HOST=x86_64-ios-darwin
+        HOST=x86_64-linux
+        CFLAGS="-march=x86-64 -msse4.2 -mpopcnt -m64 -mtune=intel"
         ASM_FLAGS="--disable-asm"
+    elif [ $ARCH = "armv7a" ];then
+        HOST=arm-linux
+        # 下面是针对armv7a架构的cpu指令优化选项，这是针对cpu的，所以每个库都可以这样设定，但是有的库比如x264的./configure文件自动添加了这些配置，就不需要手动添加
+        CFLAGS="-march=armv7-a -mcpu=cortex-a8 -mfpu=vfpv3-d16 -mfloat-abi=softfp -mthumb"
     elif [ $ARCH = "arm64" ];then
-        PLATFORM="iphoneos"
-        CFLAGS="-arch arm64 -march=armv8-a+crc+crypto -mcpu=generic"
-        CFLAGS="$CFLAGS -miphoneos-version-min=$target_ios -fembed-bitcode"
-        HOST=aarch64-ios-darwin
-    elif [ $ARCH = "arm64e" ];then
-        PLATFORM="iphoneos"
-        CFLAGS="-arch arm64e -march=armv8.3-a+crc+crypto -mcpu=generic"
-        CFLAGS="$CFLAGS -miphoneos-version-min=$target_ios -fembed-bitcode"
-        HOST=aarch64-ios-darwin
+        HOST=aaarch64-linux
+        # arm64 默认就开启了neon，所以不需要像armv7a那样手动开启
+        CFLAGS="-march=armv8-a"
     else
         echo "unsurported platform $ARCH !...."
         exit 1
     fi
+    CFLAGS="$CFLAGS -fno-integrated-as -fstrict-aliasing -fPIC -D__ANDROID_API__=${FF_ANDROID_API}"
     
-    # -target参数一般没用(用于编译一个编译器的时候才有用)
-    CFLAGS="$CFLAGS -target $HOST"
-    # -isysroot 指定了交叉编译系统的根路径，那么所有交叉编译工具或者其它相关的搜索路径都将基于此(如果不指定将按照本机/导致搜索不到路径而编译失败)
+    # --sysroot 指定了交叉编译系统的根路径，那么所有交叉编译工具或者其它相关的搜索路径都将基于此(如果不指定将按照本机/导致搜索不到路径而编译失败)
     # -I和-L指定了系统库的搜索路径，也可以在下面通过configure的配置参数--with-sysroot来指定，效果一样。
-    CFLAGS="$CFLAGS -isysroot $SDKPATH -I$SDKPATH/usr/include"
-    LDFLAGS="$CFLAGS -L${SDKPATH}/usr/lib -lc++"
-    CXXFLAGS="$CFLAGS"
+    local LL_CFLAGS="--sysroot=${FF_SYSROOT} -I${FF_SYSROOT}/usr/include -I${FF_TOOLCHAIN_PATH}/include"
+    local LL_LDFLAGS="-L${FF_SYSROOT}/usr/lib -L${FF_SYSROOT}/lib"
+    
+    CFLAGS="$CFLAGS $LL_CFLAGS"
+    CPPFLAGS="${CFLAGS}"
+    LDFLAGS="$CFLAGS $LL_LDFLAGS"
     
     # 对于符合GNU规范的configure配置脚本(比如通过Autoconf工具生成的),它一般具有如下通用配置参数选项：
     # 1、--host;表示编译出来的二进制程序(可执行程序和库)所执行的主机，如果是本机执行则无需指定。如果是交叉编译则需要指定
@@ -137,10 +263,8 @@ set_flags()
     #
     # 备注：x264 ffmpeg等非Autoconf生成的configure配置脚本以及编译器参数，可能有些不同;CFLAGS可能不同的库有些一不一样
     SYS_ROOT_CONF="--with-sysroot"
-    GAS_PL="gas-preprocessor.pl"
     if [ $lib = "x264" ];then
         SYS_ROOT_CONF="--sysroot"
-        GAS_PL="$SOURCE/tools/gas-preprocessor.pl"
     elif [ $lib = "fdk-aac" ];then
         CFLAGS="$CFLAGS -Wno-error=unused-command-line-argument-hard-error-in-future"
     else
@@ -149,19 +273,8 @@ set_flags()
         CFLAGS="$CFLAGS -Wunused-function"
     fi
     
-    case $ARCH in
-        arm64|arm64e)
-            export AS="$GAS_PL -arch aarch64 -- ${CC} ${CFLAGS}"
-        ;;
-        *)
-            if [ $lib = "x264" ];then
-                CONFIGURE_FLAGS=" $CONFIGURE_FLAGS $ASM_FLAGS "
-            fi
-            export AS="${CC} ${CFLAGS}"
-        ;;
-    esac
-    
     # 像CC AR CFLAGS CXXFLAGS等等这一类makefile用于配置编译器参数的环境变量一定要用export导入，否则不会生效
+    export HOST
     export CFLAGS
     export CXXFLAGS
     export LDFLAGS
@@ -174,14 +287,14 @@ real_do_compile()
     local lib=$2
     local ARCH=$3
     local SOURCE=$UNI_BUILD_ROOT/build/forksource/$lib
-    local PREFIX=$UNI_BUILD_ROOT/build/ios-$ARCH/$lib
+    local PREFIX=$UNI_BUILD_ROOT/build/android-$ARCH/$lib
     cd $SOURCE
     
     echo ""
     echo "build $lib $ARCH ......."
     echo "CONFIGURE_FLAGS:$CONFIGURE_FLAGS"
     echo "prefix:$PREFIX"
-    echo ""
+    echo "FF_SYSROOT:$FF_SYSROOT"
     
     set_flags $ARCH
     
@@ -193,7 +306,7 @@ real_do_compile()
         $CONFIGURE_FLAGS \
         --host=$HOST \
         --prefix=$PREFIX \
-        $SYS_ROOT_CONF=${SDKPATH} \
+        $SYS_ROOT_CONF=${FF_SYSROOT} \
 
     make -j$(get_cpu_count) && make install || exit 1
     if [ $lib = "mp3lame" ];then
@@ -209,28 +322,86 @@ real_do_compile()
 #编译x264
 do_compile_x264()
 {
-    # iOS x264 暂时无法编译动态库；会提示"ld: -read_only_relocs and -bitcode_bundle (Xcode setting ENABLE_BITCODE=YES) cannot be used together"
-    local CONFIGURE_FLAGS="--enable-static --enable-pic --disable-cli --enable-strip"
+    local CONFIGURE_FLAGS="--enable-pic --disable-cli --enable-strip "
+    # 默认为编译动态库
+    shared_enable=""
+    static_enable=""
+    # 默认生成动态库时会带版本号，这里通过匹配去掉了版本号
+    if [ $FF_COMPILE_SHARED == "TRUE" ];then
+        cd $UNI_BUILD_ROOT/build/forksource/x264
+        case "$uname" in
+            Darwin)
+                sed -i "" "s/echo \"SONAME=libx264.so.\$API\" >> config.mak/echo \"SONAME=libx264.so\" >> config.mak/g" configure
+                sed -i "" "s/ln -f -s \$(SONAME) \$(DESTDIR)\$(libdir)\/libx264.\$(SOSUFFIX)//g" Makefile
+            ;;
+            Darwin)
+                sed -i "s/echo \"SONAME=libx264.so.\$API\" >> config.mak/echo \"SONAME=libx264.so\" >> config.mak/g" configure
+                sed -i "s/ln -f -s \$(SONAME) \$(DESTDIR)\$(libdir)\/libx264.\$(SOSUFFIX)//g" Makefile
+            ;;
+            CYGWIN_NT-*)
+                sed -i "s/echo \"SONAME=libx264.so.\$API\" >> config.mak/echo \"SONAME=libx264.so\" >> config.mak/g" configure
+                sed -i "s/ln -f -s \$(SONAME) \$(DESTDIR)\$(libdir)\/libx264.\$(SOSUFFIX)//g" Makefile
+            ;;
+        esac
+        cd -
+        shared_enable="--enable-shared"
+    fi
+    if [ $FF_COMPILE_STATIC == "TRUE" ];then
+        static_enable="--enable-static"
+    fi
+    CONFIGURE_FLAGS="$CONFIGURE_FLAGS $shared_enable $static_enable"
     real_do_compile "$CONFIGURE_FLAGS" "x264" $1
 }
 
 #编译fdk-aac
 do_compile_fdk_aac()
 {
-    local CONFIGURE_FLAGS="--enable-static --enable-shared --with-pic=yes "
+    local CONFIGURE_FLAGS="--with-pic "
+    # 默认为编译动态库;fdk_aac 这个选项无效
+    shared_enable="--enable-shared"
+    static_enable=""
+    # 默认生成动态库时会带版本号，这里通过匹配去掉了版本号
+    if [ $FF_COMPILE_SHARED != "TRUE" ];then
+        shared_enable=""
+    fi
+    if [ $FF_COMPILE_STATIC == "TRUE" ];then
+        static_enable="--enable-static"
+    fi
+    CONFIGURE_FLAGS="$CONFIGURE_FLAGS $shared_enable $static_enable"
+    
+    # 遇到问题：Linux下编译时提示"error: version mismatch.  This is Automake 1.15.1"
+    # 分析原因：fdk-aac自带的生成的configure.ac和Linux系统的Automake不符合
+    # 解决方案：命令autoreconf重新配置configure.ac即可
+    if [ $uname == "Linux" ];then
+        cd $UNI_BUILD_ROOT/build/forksource/fdk-aac
+        autoreconf
+        cd -
+    fi
+    
     real_do_compile "$CONFIGURE_FLAGS" "fdk-aac" $1
 }
 #编译mp3lame
 do_compile_mp3lame()
 {
-    local CONFIGURE_FLAGS="--enable-static --disable-shared --disable-frontend --with-pic"
+    local CONFIGURE_FLAGS="--disable-frontend --with-pic=PIC"
+    # 默认为编译动态库
+    shared_enable="--enable-shared=no"
+    static_enable="--enable-static=no"
+    # 默认生成动态库时会带版本号，这里通过匹配去掉了版本号
+    if [ $FF_COMPILE_SHARED == "TRUE" ];then
+        shared_enable="--enable-shared=yes"
+    fi
+    if [ $FF_COMPILE_STATIC == "TRUE" ];then
+        static_enable="--enable-static=yes"
+    fi
+    CONFIGURE_FLAGS="$CONFIGURE_FLAGS $shared_enable $static_enable"
     real_do_compile "$CONFIGURE_FLAGS" "mp3lame" $1
 }
 #编译ass
 do_compile_ass()
 {
     # ass 依赖于freetype和fribidi，所以需要检查一下
-    local pkgpath=$UNI_BUILD_ROOT/build/ios-$1/pkgconfig
+    local pkgpath=$UNI_BUILD_ROOT/build/android-$1/pkgconfig
     if [ ! -f $pkgpath/freetype2.pc ];then
         echo "libass dependency freetype please set [freetype]=TRUE "
         exit 1
@@ -299,7 +470,7 @@ do_compile_ffmpeg()
     COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-cross-compile --enable-pic --enable-static --disable-shared --target-os=darwin"
     
     FF_SOURCE=$FF_BUILD_ROOT/build/forksource/$FF_BUILD_NAME
-    FF_PREFIX=$FF_BUILD_ROOT/build/ios-$FF_ARCH/$FF_BUILD_NAME
+    FF_PREFIX=$FF_BUILD_ROOT/build/android-$FF_ARCH/$FF_BUILD_NAME
     if [ $INTERNAL_DEBUG = "TRUE" ];then
         FF_PREFIX=/Users/apple/devoloper/mine/ffmpeg/ffmpeg-demo/demo-mac/ffmpeglib
         COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --disable-optimizations --enable-debug --disable-small";
@@ -342,10 +513,8 @@ do_compile_ffmpeg()
     
     #硬编解码，不同平台配置参数不一样
     if [ $ENABLE_GPU = "TRUE" ];then
-        # 开启Mac/IOS的videotoolbox GPU编码
+        # 开启Android的MediaCodec GPU解码;ffmpeg只支持GPU解码
         export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-encoder=h264_videotoolbox"
-        # 开启Mac/IOS的videotoolbox GPU解码
-        export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-hwaccel=h264_videotoolbox"
     fi
     
     #导入ffmpeg的外部库，这里指定外部库的路径，配置参数则转移到了config/module.sh中
@@ -405,7 +574,7 @@ function compile_external_lib_ifneed()
         fi
         
         local FF_BUILD_NAME=$lib
-        local FFMPEG_DEP_LIB=$UNI_BUILD_ROOT/build/ios-$FF_ARCH/$FF_BUILD_NAME/lib
+        local FFMPEG_DEP_LIB=$UNI_BUILD_ROOT/build/android-$FF_ARCH/$FF_BUILD_NAME/lib
 
         if [[ ${LIBFLAGS[i]} == "TRUE" ]]; then
             if [ ! -f "${FFMPEG_DEP_LIB}/lib$lib.a" ]; then
@@ -503,16 +672,16 @@ do_lipo_all () {
 if [ -z "$FF_TARGET" ]; then
     
     # 检查编译环境以及根据情况是否需要拉取源码
-    prepare_all ios $FF_ALL_ARCHS_IOS
+    prepare_all android $FF_ALL_ARCHS_ANDROID
     
-    for ARCH in $FF_ALL_ARCHS_IOS
+    for ARCH in $FF_ALL_ARCHS_ANDROID
     do
         # 设置编译环境
         set_toolchain_path $ARCH
         # 编译外部库，已经编译过则跳过。如果要重新编译，删除build下的外部库
         compile_external_lib_ifneed $ARCH
         # 编译ffmpeg
-        rm -rf build/ios-$ARCH/ffmpeg
+        rm -rf build/android-$ARCH/ffmpeg
         do_compile_ffmpeg $ARCH
     done
     
@@ -523,10 +692,10 @@ elif [[ "$FF_TARGET" == clean-* ]]; then
     # 清除对应库forksource下的源码目录和build目录
     name=${FF_TARGET#clean-*}
     rm_fork_source $name
-    rm_build ios $name $FF_ALL_ARCHS_IOS
+    rm_build android $name $FF_ALL_ARCHS_IOS
 elif [ "$FF_TARGET" == "--help" ]; then
     echo "Usage:"
-    echo "  compile-ios.sh"
-    echo "  compile-ios.sh clean-all|clean-*  (default clean ffmpeg,clean-x264 will clean x264)"
+    echo "  compile-android.sh"
+    echo "  compile-android.sh clean-all|clean-*  (default clean ffmpeg,clean-x264 will clean x264)"
     exit 1
 fi
